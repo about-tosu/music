57ca75236f161b4ed6fa2c079ce49cde7fc44403
Raghav-afk25 <raghavagarwalq12345@gmail.com>
2024-09-27 10:05:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Smaugopp/Voidmusic",
+    "https://github.com/Raghav-afk25/SpotifyxMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "stable")
 GIT_TOKEN = getenv(
