fffe1470802c15c6c431164ee2ae148bf2e82b78
Raghav-afk25 <raghavagarwalq12345@gmail.com>
2024-12-06 01:48:09 +0530

    Update Youtube.py

--- AnonXMusic/platforms/Youtube.py
+++ AnonXMusic/platforms/Youtube.py
@@ -1,13 +1,15 @@
 import asyncio
 import os
 import re
+import json
 from typing import Union
-
+import glob
+import random
+import logging
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-
 from AnonXMusic.utils.database import is_on_off
 from AnonXMusic.utils.formatters import time_to_seconds
 
